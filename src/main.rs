609e012e436fe9dbe64a9f2a9c7e73696566ac55
Mendus <alexander.mendus1@gmail.com>
2022-04-06 15:04:54 +0300

    check trait

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,9 @@ struct Person {
 trait CreatePerson {
     fn create(name: &str) -> Self;
     fn create_with_age(name: &str, age: i32) -> Self;
+    fn return_name(&self) -> &str;
+    fn return_age(&self) -> Option<i32>;
+    fn set_age(&mut self, age: i32);
 }
 
 impl CreatePerson for Person {
@@ -21,11 +24,21 @@ impl CreatePerson for Person {
             age: Some(age),
         }
     }
+    fn return_name(&self) -> &str {
+        &self.name
+    }
+    fn return_age(&self) -> Option<i32> {
+        self.age
+    }
+    fn set_age(&mut self, age: i32) {
+        self.age = Some(age);
+    }
 }
 
 fn main() {
     let person = Person::create("John");
-    let person_with_age = Person::create_with_age("John", 30);
+    let mut person_with_age = Person::create_with_age("John", 30);
+    person_with_age.set_age(40);
     println!("{}", person.age.unwrap_or_default());
     println!("{}", person_with_age.age.unwrap_or_default());
 }
