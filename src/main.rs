6efba3f0d3453f14bc3496e19e241b13ecc75411
Mendus <alexander.mendus1@gmail.com>
2022-04-17 13:44:28 +0300

    Test traits

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,41 @@
-struct Person {
-    name: String,
-    age: Option<i32>,
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
+
+trait IsLocalhost {
+    fn is_localhost(&self) -> bool;
 }
 
-trait CreatePerson {
-    fn create(name: &str) -> Self;
-    fn create_with_age(name: &str, age: i32) -> Self;
-    fn return_name(&self) -> &str;
-    fn return_age(&self) -> Option<i32>;
-    fn set_age(&mut self, age: i32);
+impl IsLocalhost for Ipv4Addr {
+    fn is_localhost(&self) -> bool {
+        Ipv4Addr::new(127, 0, 0, 1).eq(self) || Ipv4Addr::new(0, 0, 0, 0).eq(self)
+    }
 }
 
-impl CreatePerson for Person {
-    fn create(name: &str) -> Self {
-        Self {
-            name: String::from(name),
-            age: None
+impl IsLocalhost for Ipv6Addr {
+    fn is_localhost(&self) -> bool {
+        Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1).eq(self)
+    }
+}
+impl IsLocalhost for IpAddr {
+    fn is_localhost(&self) -> bool {
+        match self {
+            IpAddr::V4(a) => a.is_localhost(),
+            IpAddr::V6(a) => a.is_localhost(),
         }
     }
-    fn create_with_age(name: &str, age: i32) -> Self {
-        Self {
-            name: String::from(name),
-            age: Some(age),
-        }
-    }
-    fn return_name(&self) -> &str {
-        &self.name
-    }
-    fn return_age(&self) -> Option<i32> {
-        self.age
-    }
-    fn set_age(&mut self, age: i32) {
-        self.age = Some(age);
+}
+
+trait Print {
+    fn print(&self);
+}
+
+impl<T: std::fmt::Debug> Print for T {
+    fn print(&self) {
+        println!("{:?}", self);
     }
 }
 
 fn main() {
-    let mut person = Person::create("John");
-    let person_with_age = Person::create_with_age("John", 30);
-
-    match person.age {
-        Some(age) => println!("Check age: {}", age),
-        None => person.set_age(0),
-    }
-
-    if person.age.is_none() {
-        person.set_age(18);
-    }
-
-    println!("{}", person.age.unwrap());
-    println!("{}", person_with_age.age.unwrap_or_default());
+    let ip_address = Ipv4Addr::new(0, 0, 0, 0);
+    ip_address.print();
+    ip_address.is_localhost().print();
 }
